6a727165fbcf32e92e4569904a6a5c8f9ab274e1
Degraft Frimpong <degraft@degraft.degraft>
2024-05-15 19:54:56 -0400

    connectedUsers added

---
+++ server/socket/connectedUsers.js
@@ -0,0 +1,58 @@
+const connectedUsers=new Map();
+let io=null;
+
+// connect new user
+const addNewConnectedUser=({socketId,userId})=>{
+    connectedUsers.set(socketId,{userId});
+}
+
+
+const removeConneted=({socketId})=>{
+    if(connectedUsers.has(socketId)){
+        connectedUsers.delete(socketId);
+    }
+}
+
+// get all conneted users
+const getActiveConnections=(userId)=>{
+    const activeConnections=[];
+    connectedUsers.forEach((value,key)=>{
+        if(value.userId===userId){
+            activeConnections.push(key);
+        }
+    });
+
+    return activeConnections;
+}
+
+const getOnlineUsers=()=>{
+    const onlineUsers=[];
+    connectedUsers.forEach((value,key)=>{
+        onlineUsers.push({
+            userId:value.userId,
+            socketId:key,
+        })
+    })
+    return onlineUsers;
+}
+
+// setting server instance
+const setServerSocketInstance=(ioInstance)=>{
+    io=ioInstance;
+}
+
+// get server instance
+const getServerSocketInstance=()=>{
+    return io;
+}
+
+
+module.exports={
+    addNewConnectedUser,
+    removeConneted,
+    getOnlineUsers,
+    getActiveConnections,
+    setServerSocketInstance,
+    getServerSocketInstance
+}
+
