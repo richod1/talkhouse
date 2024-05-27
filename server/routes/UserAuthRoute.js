dabbbe236c0d2802ae2820e79a4bfa1b74874635
Degraft Frimpong <frimpsup@gmail.com>
2024-05-27 11:42:20 -0400

    userAuth route added

--- server/routes/UserAuthRoute.js
+++ server/routes/UserAuthRoute.js
@@ -1,9 +1,10 @@
 const {register,login}=require("../controllers/authController")
 const express=require("express")
-const route=express.Router()
+const router=express.Router()
 constJoi=require("joi")
 const validator=require("express-joi-validation").createValidator({})
-const requireSocketAuth=require("../middleware/requireSocketAuth")
+// const requireSocketAuth=require("../middleware/requireSocketAuth")
+const requireAuth=require("../middleware/requireAuth")
 
 const registerSchema=Joi.Object({
     username:Joi.string().min(3).max(12).required(),
@@ -15,3 +16,12 @@ const loginSchema=Joi.Object({
     password:Joi.string().min(6).required(),
     email:Joi.string().email().required()
 })
+
+
+// test route
+
+router.get("/test",requireAuth,(req,res)=>{
+    res.send(`hello, ${req.user.email}`);
+});
+
+module.exports=router;
